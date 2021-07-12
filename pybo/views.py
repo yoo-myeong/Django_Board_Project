65eef9f2e962041491f233d9334d6816668d301d
yoo-myeong <yoou201@naver.com>
2021-07-12 11:54:49 +0900

    주석제거

--- pybo/views.py
+++ pybo/views.py
@@ -3,15 +3,25 @@ from .forms import QuestionForm, AnswerForm
 from .models import Question, Answer
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
+
 
 
 def index(request):
     """
-    pybo 목록 출력
+    pybo 목록출력
     """
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
 
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
